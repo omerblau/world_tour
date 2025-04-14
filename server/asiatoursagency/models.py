136ca20bd8c8ad294e839dce12b0655e84f7637b
Omer Blau <omerbl2@mta.ac.il>
2025-04-14 18:43:36 +0300

    Add initial migration and model definition for Tour in asiatoursagency

--- server/asiatoursagency/models.py
+++ server/asiatoursagency/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+    origin_country = models.CharField(max_length=100)
+    destination_country = models.CharField(max_length=100)
+    number_of_nights = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
